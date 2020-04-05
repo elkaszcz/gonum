30cc09b0c075a4afbf60aac3f3d550a6c2338bb3
elkaszcz <lukasz.kaszczyszyn@gmail.com>
2020-04-05 20:19:32 +0200

    add colors

--- calc/math.go
+++ calc/math.go
@@ -1,17 +1,22 @@
 package calc
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/fatih/color"
+)
 
 // Add returns sum of given integers
 func Add(numbers ...int) (int, error) {
 	sum := 0
 
 	if len(numbers) < 3 {
-		return sum, errors.New("Provide more than 3 numbers")
-	} else {
-		for _, num := range numbers {
-			sum = sum + num
-		}
+		errorMessage := color.RedString("Provide more than 3 numbers")
+		return sum, errors.New(errorMessage)
 	}
+	for _, num := range numbers {
+		sum = sum + num
+	}
+
 	return sum, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/elkaszcz/gonum/v2
 
 go 1.14
+
+require github.com/fatih/color v1.9.0
