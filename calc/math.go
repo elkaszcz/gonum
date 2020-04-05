aba2d4242dbd0ef4c507a3eb27d006dc6de55fe7
elkaszcz <lukasz.kaszczyszyn@gmail.com>
2020-04-05 19:48:47 +0200

    add function must have more than 3 args

--- calc/math.go
+++ calc/math.go
@@ -1,11 +1,17 @@
 package calc
 
+import "errors"
+
 // Add returns sum of given integers
-func Add(numbers ...int) int {
+func Add(numbers ...int) (int, error) {
 	sum := 0
 
-	for _, num := range numbers {
-		sum = sum + num
+	if len(numbers) < 3 {
+		return sum, errors.New("Provide more than 3 numbers")
+	} else {
+		for _, num := range numbers {
+			sum = sum + num
+		}
 	}
-	return sum
+	return sum, nil
 }
